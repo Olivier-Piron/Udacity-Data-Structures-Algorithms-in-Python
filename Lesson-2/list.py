682cdf45d4311bfb308cd9af40ff8f33bf1d98b2
Olivier-BeCode <76862461+Olivier-BeCode@users.noreply.github.com>
2021-07-02 13:11:33 +0200

    Lesson 1+2

---
+++ Lesson-2/list.py
@@ -0,0 +1,12 @@
+# For example, inserting into a list is easy (happens in constant time). 
+# However, inserting into an array is O(n), since you may need to shift elements to make space for the one 
+# you're inserting, or even copy everything to a new array if you run out of space. Thus, inserting into a 
+# Python list is actually O(n), while operations that search for an element at a particular spot are O(1). 
+# You can see the runtime of other list operations here.
+#  https://wiki.python.org/moin/TimeComplexity
+
+# Python is a "higher level" programming language, so you can accomplish a task with little code. 
+# However, there's a lot of code built into the infrastructure in this way that causes your code to actually 
+# run much more slowly than you'd think. 
+
+# The runtime to find the length of a python liwt is O(1)
